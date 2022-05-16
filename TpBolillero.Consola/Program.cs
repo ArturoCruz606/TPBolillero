33ccff63709ca17541a96d3ed8f542a7a714a05a
luchoxx87 <ar7ur0321@gmail.com>
2022-05-16 16:25:34 -0300

    Finnalizacion
    asdasdfadsf

--- TpBolillero.Consola/Program.cs
+++ TpBolillero.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using TpBolillero.Core;
 
@@ -8,17 +9,29 @@ namespace TpBolillero.Consola
     {
         static void Main(string[] args)
         {
-            Bolillero bolillero = new Bolillero(10);
+            Stopwatch cronometro = new Stopwatch();
+            int repeticiones = 1000000000;
+            Bolillero bolillero = new Bolillero(100);
             bolillero.Azar = new Aleatorio();
             Simulacion simulacion = new Simulacion();
-            List<byte> lista = new List<byte>{1};
-            var inicio = DateTime.Now;
-            Console.WriteLine($"Simulando sin hilos{inicio}");
-            System.Console.WriteLine(simulacion.SimularSinHilos(bolillero,lista, 300000)); 
-            var fin = DateTime.Now;
-            Console.WriteLine(fin);
-            Console.WriteLine("Simulando con hilos");
-            simulacion.SimularConHilos(bolillero, lista, 300000, 6);
+            List<byte> lista = new List<byte>{1, 7, 15 , 2, 40, 72};
+            cronometro.Start();
+            Console.WriteLine($"Simulando sin hilos");
+            System.Console.WriteLine(simulacion.SimularSinHilos(bolillero,lista, repeticiones)); 
+            cronometro.Stop();
+            TimeSpan tt = cronometro.Elapsed;
+            string tiempotranscurrido = String.Format("{0:00}:{1:00}:{2:00}",
+            tt.Hours, tt.Minutes, tt.Seconds);
+            System.Console.WriteLine(tiempotranscurrido);
+            cronometro.Reset();
+            cronometro.Start();
+            Console.WriteLine($"Simulando con hilos");
+            System.Console.WriteLine(simulacion.SimularConHilos(bolillero, lista, repeticiones, 6));
+            cronometro.Stop();
+            tt = cronometro.Elapsed;
+            tiempotranscurrido = String.Format("{0:00}:{1:00}:{2:00}",
+            tt.Hours, tt.Minutes, tt.Seconds);
+            System.Console.WriteLine(tiempotranscurrido);
         }
     }
 }
