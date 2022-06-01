49276024e6b0eee81ba9012bde1ee606e4900de1
ArturoCruz606 <ar7ur0321@gmail.com>
2022-06-01 15:02:06 -0300

    Finalizacion de SimulacionConHilosAsync
    alsjda

--- TpBolillero.Consola/Program.cs
+++ TpBolillero.Consola/Program.cs
@@ -2,12 +2,13 @@
 using System.Diagnostics;
 using System.Collections.Generic;
 using TpBolillero.Core;
+using System.Threading.Tasks;
 
 namespace TpBolillero.Consola
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Stopwatch cronometro = new Stopwatch();
             int repeticiones = 1000000000;
@@ -15,6 +16,7 @@ namespace TpBolillero.Consola
             bolillero.Azar = new Aleatorio();
             Simulacion simulacion = new Simulacion();
             List<byte> lista = new List<byte>{1, 7, 15 , 2, 40, 72};
+            
             cronometro.Start();
             Console.WriteLine($"Simulando sin hilos");
             System.Console.WriteLine(simulacion.SimularSinHilos(bolillero,lista, repeticiones)); 
@@ -23,10 +25,21 @@ namespace TpBolillero.Consola
             string tiempotranscurrido = String.Format("{0:00}:{1:00}:{2:00}",
             tt.Hours, tt.Minutes, tt.Seconds);
             System.Console.WriteLine(tiempotranscurrido);
+            
             cronometro.Reset();
             cronometro.Start();
             Console.WriteLine($"Simulando con hilos");
             System.Console.WriteLine(simulacion.SimularConHilos(bolillero, lista, repeticiones, 6));
+            cronometro.Stop();
+            tt = cronometro.Elapsed;
+            tiempotranscurrido = String.Format("{0:00}:{1:00}:{2:00}",
+            tt.Hours, tt.Minutes, tt.Seconds);
+            System.Console.WriteLine(tiempotranscurrido);
+
+            cronometro.Reset();
+            cronometro.Start();
+            Console.WriteLine($"Simulando con hilos");
+            System.Console.WriteLine(await simulacion.SimularConHilosAsync(bolillero, lista, repeticiones, 6));
             cronometro.Stop();
             tt = cronometro.Elapsed;
             tiempotranscurrido = String.Format("{0:00}:{1:00}:{2:00}",
--- TpBolillero.Core/Simulacion.cs
+++ TpBolillero.Core/Simulacion.cs
@@ -21,5 +21,15 @@ namespace TpBolillero.Core
             Task<long>.WaitAll(tarea);
             return tarea.Sum(t => t.Result);
         }
+        public async Task<long> SimularConHilosAsync(Bolillero bolillero, List<byte> jugada, int cantidadSimulaciones, int cantidaddehilos){
+            Task<long>[] tarea = new Task<long>[cantidaddehilos];
+            for (int i = 0; i < cantidaddehilos; i++)
+            {
+                var clon = (Bolillero)bolillero.Clone();
+                tarea[i] = Task<long>.Run(() => SimularSinHilos(clon, jugada, cantidadSimulaciones / cantidaddehilos));
+            }
+            await Task<long>.WhenAll(tarea);
+            return tarea.Sum(t => t.Result);
+        }
     }
 }
